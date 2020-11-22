339713949c8f868b660641110a5df56b3f1cf543
Mario Conrad <mario.conrad@gmail.com>
2020-11-22 20:41:49 +0100

    changed setup.py

--- Snips/__init__.py
+++ Snips/__init__.py
@@ -1,4 +1,14 @@
+__version__ = "1.0.0"
+
 from .funcs_testing import below2
 from .funcs_testing import format_data_for_display
 from .funcs_testing import hello
 from .funcs_testing import hello_raises
+
+__all__ = [
+    '__version__',
+    'below2',
+    'format_data_for_display',
+    'hello',
+    'hello_raises',
+]
